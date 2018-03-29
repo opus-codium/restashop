1626496e45ed98822439a3b2460c0716a08bc3b0
Romuald Conty <neomilium@gmail.com>
2018-03-29 12:25:31 +0200

    spec: refactor tests

--- spec/restashop_spec.rb
+++ spec/restashop_spec.rb
@@ -1,108 +1,79 @@
 require 'restashop'
 
-def stub_prestashop_resources
-  json = File.read(File.expand_path('api.json', __dir__))
-  stub_request(:get, 'http://prestashop.com/api?output_format=JSON')
+def stub_prestashop(path, json)
+  stub_request(:get, "http://prestashop.com/#{path}")
     .with(headers: {
             'Accept' => '*/*'
           })
     .to_return(status: 200, body: json, headers: {})
 end
 
+def stub_prestashop_resources
+  json = File.read(File.expand_path('api.json', __dir__))
+  stub_prestashop 'api?output_format=JSON', json
+end
+
+def stub_prestashop_suppliers
+  json = '{"suppliers":[{"id":2},{"id":4},{"id":42}]}'
+  stub_prestashop 'api/suppliers?output_format=JSON', json
+end
+
+def stub_prestashop_suppliers_full
+  json = '{"suppliers":[' \
+         '{"id":2,"name":"Mega supplier"},' \
+         '{"id":4,"name":"Awesome supplier"},' \
+         '{"id":42,"name":"Super supplier"}' \
+         ']}'
+  stub_prestashop 'api/suppliers?output_format=JSON&display=full', json
+end
+
+def stub_prestashop_supplier_42
+  json = '{"supplier":{"id":42, "name":"Super supplier"}}'
+  stub_prestashop 'api/suppliers/42?output_format=JSON', json
+end
+
 RSpec.describe Restashop do
   context 'with fully working setup' do
-    it 'fetch resources and provide associated symbols' do
-      stub_prestashop_resources
-      restashop = Restashop.new(URI.parse('http://prestashop.com/api').to_s,
-                                user: 'XXX')
+    let(:restashop) do
+      Restashop.new(URI.parse('http://prestashop.com/api').to_s,
+                    user: 'XXX')
+    end
 
+    before do
+      stub_prestashop_resources
+      stub_prestashop_suppliers
+      stub_prestashop_suppliers_full
+      stub_prestashop_supplier_42
+    end
+
+    it 'fetch resources and provide associated symbols' do
       expect(restashop.resources).to include 'shops'
       expect(restashop.shops.class).to eq Shops
     end
 
-    it 'list resource IDs' do
-      stub_prestashop_resources
-      stub_request(:get, 'http://prestashop.com/api/suppliers?output_format=JSON')
-        .with(headers: {
-                'Accept' => '*/*'
-              })
-        .to_return(status: 200,
-                   body: '{"suppliers":[{"id":10},{"id":32},{"id":42}]}',
-                   headers: {})
-
-      restashop = Restashop.new(URI.parse('http://prestashop.com/api').to_s,
-                                user: 'XXX')
-
-      expect(restashop.suppliers.list).to eq [10, 32, 42]
+    it 'lists resource IDs' do
+      expect(restashop.suppliers.list).to eq [2, 4, 42]
     end
 
-    it 'count resource IDs' do
-      stub_prestashop_resources
-      stub_request(:get, 'http://prestashop.com/api/suppliers?output_format=JSON')
-        .with(headers: {
-                'Accept' => '*/*'
-              })
-        .to_return(status: 200,
-                   body: '{"suppliers":[{"id":10},{"id":32},{"id":42}]}',
-                   headers: {})
-
-      restashop = Restashop.new(URI.parse('http://prestashop.com/api').to_s,
-                                user: 'XXX')
-
+    it 'counts resource IDs' do
       expect(restashop.suppliers.count).to eq 3
     end
 
     it 'returns all resource items in a row' do
-      stub_prestashop_resources
-      stub_request(:get, 'http://prestashop.com/api/suppliers?display=full&output_format=JSON')
-        .with(headers: {
-                'Accept' => '*/*'
-              })
-        .to_return(status: 200,
-                   body: '{"suppliers":[' \
-                   '{"id":3,"name":"Super supplier"},' \
-                   '{"id":4,"name":"Mega supplier"}' \
-                   ']}',
-                   headers: {})
-
-      restashop = Restashop.new(URI.parse('http://prestashop.com/api').to_s,
-                                user: 'XXX')
-
       suppliers = restashop.suppliers.all
       expect(suppliers.class).to eq Array
-      expect(suppliers.count).to eq 2
+      expect(suppliers.count).to eq 3
       expect(suppliers[0].class).to eq Supplier
     end
 
     it 'find a resource using ID' do
-      stub_prestashop_resources
-      stub_request(:get, 'http://prestashop.com/api/suppliers/42?output_format=JSON')
-        .with(headers: {
-                'Accept' => '*/*'
-              })
-        .to_return(status: 200,
-                   body: '{"supplier":{"id":42, "name":"Super supplier"}}',
-                   headers: {})
-      restashop = Restashop.new(URI.parse('http://prestashop.com/api').to_s,
-                                user: 'XXX')
       supplier = restashop.suppliers.find(42)
       expect(supplier.class).to eq Supplier
       expect(supplier.name).to eq 'Super supplier'
     end
 
     it 'show a resource keys' do
-      stub_prestashop_resources
-      stub_request(:get, 'http://prestashop.com/api/suppliers/42?output_format=JSON')
-        .with(headers: {
-                'Accept' => '*/*'
-              })
-        .to_return(status: 200,
-                   body: '{"supplier":{"id":42, "name":"Super supplier"}}',
-                   headers: {})
-      restashop = Restashop.new(URI.parse('http://prestashop.com/api').to_s,
-                                user: 'XXX')
       supplier = restashop.suppliers.find(42)
-      expect(supplier.keys.count).to eq 2
       expect(supplier.keys).to include 'id'
       expect(supplier.keys).to include 'name'
     end
